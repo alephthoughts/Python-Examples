6b2512a5cb79e25d45394d8c04c73367defb79d0
alephthoughts <abhisheksharma09@gmail.com>
2019-08-25 21:36:40 +0530

    Day 6

---
+++ sortArraybyElementFrequency.py
@@ -0,0 +1,19 @@
+from collections import Counter
+from itertools import chain
+
+def frequency_sort(items):
+    most_common = Counter(items).most_common()
+    return list(chain.from_iterable([elem] * freq for elem, freq in most_common))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(frequency_sort([4, 6, 2, 2, 6, 4, 4, 4])) == [4, 4, 4, 4, 6, 6, 2, 2]
+    assert list(frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob'])) == ['bob', 'bob', 'bob', 'carl', 'alex']
+    assert list(frequency_sort([17, 99, 42])) == [17, 99, 42]
+    assert list(frequency_sort([])) == []
+    assert list(frequency_sort([1])) == [1]
+    
