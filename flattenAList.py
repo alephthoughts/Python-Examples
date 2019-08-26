f6c7adbc777ca468a9eb3d8ed098dae4f5276fc4
alephthoughts <abhisheksharma09@gmail.com>
2019-08-26 12:20:31 +0530

    Day 7

---
+++ flattenAList.py
@@ -0,0 +1,15 @@
+def flat_list(array):
+    data = []
+    for item in array:
+        if type(item) == list:
+            data.extend(flat_list(item))
+        else:
+            data.append(item)
+    return data
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
