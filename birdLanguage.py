175f69c0d36943f701cf4fd7b86c0e0b30d9b8a8
alephthoughts <abhisheksharma09@gmail.com>
2019-09-02 10:23:06 +0530

    Day 10

---
+++ birdLanguage.py
@@ -0,0 +1,25 @@
+
+VOWELS = "aeiouy"
+
+def translate(phrase):
+    human_phrase = []
+    i = 0
+    while i < len(phrase):
+        human_phrase.append(phrase[i])
+        if phrase[i] in VOWELS:
+            i = i + 3
+        elif phrase[i] == ' ':
+            i = i + 1
+        else:
+            i = i + 2
+    return ''.join(human_phrase) 
+
+if __name__ == '__main__':
+    print("Example:")
+    print(translate("hieeelalaooo"))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert translate("hieeelalaooo") == "hello", "Hi!"
+    assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin", "Joey?"
+    assert translate("aaa bo cy da eee fe") == "a b c d e f", "Alphabet"
+    assert translate("sooooso aaaaaaaaa") == "sos aaa", "Mayday, mayday"
