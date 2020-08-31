48cc93b0788c6814f95a460049aa5c5fb12585d0
alephthoughts <alephthoughts@gmail.com>
2020-08-31 18:35:45 +0530

    split list

---
+++ split-list.py
@@ -0,0 +1,17 @@
+import math
+def split_list(items: list) -> list:
+    # your code here
+    l = math.ceil(len(items)/2)
+    return [items[:l], items[l:]]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(split_list([1, 2, 3, 4, 5, 6]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+    assert split_list([1, 2, 3]) == [[1, 2], [3]]
+    assert split_list([1, 2, 3, 4, 5]) == [[1, 2, 3], [4, 5]]
+    assert split_list([1]) == [[1], []]
+    assert split_list([]) == [[], []]
